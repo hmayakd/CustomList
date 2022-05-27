81e1ebd5a01e25a5220af6c3fb8fb7227505bcb4
Hmayak Davtyan <davtyan.hmayak@gmail.com>
2022-05-27 19:31:18 +0400

    First push.

--- CustomList/Program.cs
+++ CustomList/Program.cs
@@ -9,6 +9,9 @@ namespace CustomList
         {
             _List _list = new _List();
             _list.Add(2);
+            _list.Add(22);
+            _list.Add(222);
+
             int a = _list[0];
             _list[0] = 12;
             Console.WriteLine(_list.Capacity);
@@ -28,6 +31,8 @@ namespace CustomList
             {
                 arr1[i] = i + 2;
             }
+            int[] arr2 = new int[10]; 
+            int[] arr3 = new int[11];
             _List _list2 = new _List(arr);
             _List _list3 = new _List();
             _list3.AddRange(arr);
@@ -40,8 +45,8 @@ namespace CustomList
             _list5.AddRange(arr);
             _list5.InsertRange(2, arr1);
 
-            _list.Remove(2);
-            _list1.RemoveAt(0);
+            _list.Remove(12);
+            _list.RemoveAt(1);
             _list2.RemoveRange(1, 2);
 
             _list3.Reverse();
@@ -49,9 +54,9 @@ namespace CustomList
 
             _list5.Clear();
 
-            bool c = _list5.Contains(3);
+            bool c = _list4.Contains(3);
 
-            _list.CopyTo(arr);
+            _list4.CopyTo(arr3);
             _list1.CopyTo(arr, 3);
             _list2.CopyTo(2, arr, 3, 4);
 
@@ -63,7 +68,7 @@ namespace CustomList
             b = _list4.LastIndexOf(3, 2);
             b = _list5.LastIndexOf(3, 2, 3);
 
-            int[] arr2 = new int[10];
+            
             arr2 = _list.ToArray();
 
             foreach (var item in _list2)
